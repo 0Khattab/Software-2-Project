aeae567d193fc077491bbe673fd3e31042193e0c
Omar-Nour-eldeen <omar.nour.github@gmail.com>
2026-05-07 23:18:46 +0300

    link product-service with gateway

--- E-Commerce_Project/product-service/src/main/java/com/product/product/Models/Category.java
+++ E-Commerce_Project/product-service/src/main/java/com/product/product/Models/Category.java
@@ -34,7 +34,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false, unique = true, length = 50)
+    @Column(name = "name", nullable = false, length = 50)
     private String name;
 
     @CreationTimestamp
